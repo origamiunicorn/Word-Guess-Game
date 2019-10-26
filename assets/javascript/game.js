d75e8ebbc55da59a3250e5107a72ce57161f2efa
origami unicorn <rvwright@gmail.com>
2019-10-25 23:51:16 -0700

    Added in variables for words to guess, a random selection of words to guess, a blank array for guessing, and unknown letters that starts off as the length of the random word. Evaluating for and while loops right now.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,38 @@
 
 var keyLog = document.getElementById("guessedLettersDiv");
-var alphaList = /[a-zA-Z]/;
+// Come back to alphaList, not working as I would like.
+var alphaList = /^[a-zA-Z]*$/;
+var wordsToGuess = ["salchow", "toe loop", "axel jump", "biellmann position", "camel spin", "choctaw", "combination spin", "crossover", "death spiral", "edge jump", "flip jump", "flying spin", "hydrant lift", "layback spin", "jump sequence", "loop jump", "lutz jump", "overhead lifts", "rocker", "scratch spin", "twizzles", "sit spin", "spiral", "swizzle", "throw jump", "upright spin"]
+var wordsRandom = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
+var guessArray = [];
+var unknownLetters = wordsRandom.length;
 
 // Keyup or keydown, but not keypress. Keyup for single detection(?)
+while (unknownLetters > 0) {
+    document.addEventListener("keyup", event => {
+        var key = event.key.toLowerCase();
+        var str = String.fromCharCode(event.keyCode);
+        // var keyLogText = document.getElementById("guessedLettersDiv").innerText;
 
-document.addEventListener("keyup", event => {
-    var key = event.key.toLowerCase();
-    var str = String.fromCharCode(event.keyCode);
-    // var keyLogText = document.getElementById("guessedLettersDiv").innerText;
+        if (alphaList.test(key) === false) {
+            return;
+        } else {
+            keyLog.innerHTML += str.toUpperCase() + " ";
+        }
+        console.log(key);
 
-    if (alphaList.test(key) === false) {
-        return;
-    } else {
-        keyLog.innerHTML += str.toUpperCase() + " ";
-    }
-    console.log(key);
-});
+        for (var i = 0; i < wordsRandom.length; i++) {
+            guessArray[i] = " _ ";
+        }
+
+        for (var l = 0; l < wordsRandom.length; l++) {
+            if (wordsRandom[l] === event) {
+                guessArray[l] = event;
+                unknownLetters--;
+            }
+        }
+    });
+}
 
 
 
