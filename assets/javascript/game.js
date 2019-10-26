bfeb6aa91ba7e2886fb4b9993d957e785aae8671
origami unicorn <rvwright@gmail.com>
2019-10-26 00:29:12 -0700

    Threw some JS out to reorganise as continued reading references, moved up for loop handling word generation to blanks so it wasn't waiting on a keyup to appear. Added a join to replace commas that had been showing between letters for the indices of the unsolved word in guessArray.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,39 @@
-
+var winsTracking = document.getElementById("winsDiv");
+var currentWord = document.getElementById("currentWordDiv");
+var guessTracking = document.getElementById("guessesLeftDiv");
 var keyLog = document.getElementById("guessedLettersDiv");
-// Come back to alphaList, not working as I would like.
-var alphaList = /^[a-zA-Z]*$/;
+var alphaList = /^[a-zA-Z]*$/; // Return to later, not satisfied.
 var wordsToGuess = ["salchow", "toe loop", "axel jump", "biellmann position", "camel spin", "choctaw", "combination spin", "crossover", "death spiral", "edge jump", "flip jump", "flying spin", "hydrant lift", "layback spin", "jump sequence", "loop jump", "lutz jump", "overhead lifts", "rocker", "scratch spin", "twizzles", "sit spin", "spiral", "swizzle", "throw jump", "upright spin"]
 var wordsRandom = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
 var guessArray = [];
 var unknownLetters = wordsRandom.length;
+var wins = 0;
 
-// Keyup or keydown, but not keypress. Keyup for single detection(?)
-while (unknownLetters > 0) {
-    document.addEventListener("keyup", event => {
-        var key = event.key.toLowerCase();
-        var str = String.fromCharCode(event.keyCode);
-        // var keyLogText = document.getElementById("guessedLettersDiv").innerText;
-
-        if (alphaList.test(key) === false) {
-            return;
-        } else {
-            keyLog.innerHTML += str.toUpperCase() + " ";
-        }
-        console.log(key);
-
-        for (var i = 0; i < wordsRandom.length; i++) {
-            guessArray[i] = " _ ";
-        }
-
-        for (var l = 0; l < wordsRandom.length; l++) {
-            if (wordsRandom[l] === event) {
-                guessArray[l] = event;
-                unknownLetters--;
-            }
-        }
-    });
+// Select a random word, set it to be filled with blank spaces. Like my brain! It's a veritable swiss cheese in here...
+for (var i = 0; i < wordsRandom.length; i++) {
+    guessArray[i] = "_";
+    currentWord.innerHTML = guessArray.join(" ");
 }
+
+// Keyup or keydown, but not keypress. 
+document.addEventListener("keyup", event => {
+    var key = event.key.toLowerCase();
+    var str = String.fromCharCode(event.keyCode);
+
+    if (alphaList.test(key) === false) {
+        return;
+    } else {
+        keyLog.innerHTML += str.toUpperCase() + " ";
+    }
+    console.log(key);
+
+    // for (var l = 0; l < wordsRandom.length; l++) {
+    //     if (wordsRandom[l] === event) {
+    //         guessArray[l] = event;
+    //         unknownLetters--;
+    //     }
+    // }
+});
 
 
 
