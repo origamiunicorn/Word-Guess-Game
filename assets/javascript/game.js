463deefc3becafcb57e429363839a381810c8406
origami unicorn <rvwright@gmail.com>
2019-10-26 04:39:33 -0700

    Attempting a code rewrite for clarity, starting early with use of Set in order to see about keeping alphabet repetition to zero.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,109 @@
-var winsTracking = document.getElementById("winsDiv");
-var currentWord = document.getElementById("currentWordDiv");
-var guessTracking = document.getElementById("guessesLeftDiv");
 var keyLog = document.getElementById("guessedLettersDiv");
-var alphaList = /^[a-zA-Z]*$/; // Return to later, not satisfied.
-var wordsToGuess = ["salchow", "toe loop", "axel jump", "biellmann position", "camel spin", "choctaw", "combination spin", "crossover", "death spiral", "edge jump", "flip jump", "flying spin", "hydrant lift", "layback spin", "jump sequence", "loop jump", "lutz jump", "overhead lifts", "rocker", "scratch spin", "twizzles", "sit spin", "spiral", "swizzle", "throw jump", "upright spin"]
+
+var wordsToGuess = ["salchow", "toeloop", "axeljump", "biellmann", "camelspin"]
 var wordsRandom = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
-var numberGuesses = Math.round(wordsRandom.length * 1.5);
+var lettersArray = new Set();
 var guessArray = [];
-var unknownLetters = wordsRandom.length;
-var wins = 0;
+var alphaList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-// Select a random word, set it to be filled with blank spaces. Like my brain! It's a veritable swiss cheese in here...
-for (var i = 0; i < wordsRandom.length; i++) {
-    guessArray[i] = "_";
-    currentWord.innerHTML = guessArray.join(" ");
-    currentWord.innerText += "\n" + wordsRandom;
-}
-
-
-// Keyup or keydown, but not keypress. 
 document.addEventListener("keyup", event => {
-    var key = event.key.toLowerCase();
+    var keyGuess = event.key.toLowerCase();
     var str = String.fromCharCode(event.keyCode);
 
-    if (alphaList.test(key) === false) {
+    if (alphaList.indexOf(keyGuess) === -1) return;
+    if (lettersArray.has(keyGuess) === true) {
         return;
     } else {
-        keyLog.innerHTML += str.toUpperCase() + " ";
+        lettersArray.add(keyGuess);
     }
-    console.log(key);
+    keyLog.innerHTML += str.toUpperCase() + " ";
+    console.log(str);
+})
 
-    // for (var l = 0; l < wordsRandom.length; l++) {
-    //     if (wordsRandom[l] === event) {
-    //         guessArray[l] = event;
-    //         unknownLetters--;
-    //     }
-    // }
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var winsTracking = document.getElementById("winsDiv");
+// var currentWord = document.getElementById("currentWordDiv");
+// var guessTracking = document.getElementById("guessesLeftDiv");
+// var keyLog = document.getElementById("guessedLettersDiv");
+// var alphaList = /^[a-zA-Z]*$/; // Return to later, not satisfied. 
+// var wordsToGuess = ["salchow", "toeloop", "axeljump", "biellmann", "camelspin"]
+// // , "choctaw", "combination spin", "crossover", "deathspiral", "edgejump", "flipjump", "flying spin", "hydrantlift", "laybackspin", "jumpsequence", "loopjump", "lutzjump", "overheadlifts", "rocker", "scratchspin", "twizzles", "sitspin", "spiral", "swizzle", "throwjump", "uprightspin"
+// var wordsRandom = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
+// var totalGuesses = Math.round(wordsRandom.length * 1.5);
+// var guessArray = [];
+// var lettersLeft = wordsRandom.length;
+// var wins = 0;
+// var guessedSet = new Set();
+
+// for (var i = 0; i < wordsRandom.length; i++) {
+//     guessArray[i] = "_";
+//     currentWord.innerHTML = guessArray.join(" ");
+//     currentWord.innerText += "\n" + wordsRandom;
+//     guessTracking.innerText = "Number of guesses remaining: " + totalGuesses;
+// }
+
+// document.addEventListener("keyup", event => {
+//     var keyGuess = event.key.toLowerCase();
+//     var str = String.fromCharCode(event.keyCode);
+
+//     guessTracking.innerText = "Number of guesses remaining: " + totalGuesses--;
+
+//     if (alphaList.test(keyGuess) === false) {
+//         return;
+//     }
+//     else if (keyGuess.indexOf(guessedSet) === true) {
+//         return;
+//     } else {
+//         keyLog.innerHTML += str.toUpperCase() + " ";
+//     }
+//     console.log(keyGuess);
+
+//     guessedSet.add(keyGuess);
+//     console.log(guessedSet);
+
+//     // var checkKey = wordsRandom.split("");
+//     // console.log(checkKey);
+
+//     if (wordsRandom.indexOf(str) != -1) {
+//         for (var k = 0; k < wordsRandom.length; k++) {
+//             if (wordsRandom[k] == str)
+//                 guessArray[k] = wordsRandom[k];
+//         }
+//     }
+
+// });
 
 
 
