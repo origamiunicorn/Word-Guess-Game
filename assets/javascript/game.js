1ee639a195a4d78248fa55813a7f4000de5df9ff
origami unicorn <rvwright@gmail.com>
2019-10-26 08:58:36 -0700

    Third time hauling all JS down and reorganising in order to overhaul again, reading through many sources on ways to return information and what better DOM practices were. Still using Set to limit repetition. Currently have wins tallying, word-specific guessing chances counting down, all caps Letters Already Guessed manifesting.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,134 @@
+var currentWord = document.getElementById("currentWordDiv");
+var guessTracking = document.getElementById("guessesLeftDiv");
 var keyLog = document.getElementById("guessedLettersDiv");
+var winTracking = document.getElementById("winsDiv");
 
 var wordsToGuess = ["salchow", "toeloop", "axeljump", "biellmann", "camelspin"]
-var wordsRandom = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
-var lettersArray = new Set();
+
+var wordsRandom = "";
+var wordsRandomStr = [];
+var totalGuessLength = 0;
+var setArray = new Set();
 var guessArray = [];
 var alphaList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var guessedAlpha = [];
 
-document.addEventListener("keyup", event => {
-    var keyGuess = event.key.toLowerCase();
-    var str = String.fromCharCode(event.keyCode);
+var winWin = 0;
+var loseLose = 0;
+var totalGuesses;
 
-    if (alphaList.indexOf(keyGuess) === -1) return;
-    if (lettersArray.has(keyGuess) === true) {
+function helloDarkness() {
+    wordsRandom = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
+    totalGuesses = Math.round(wordsRandom.length * 1.5);
+    wordsRandomStr = wordsRandom.split("");
+    totalGuessLength = wordsRandomStr.length;
+    guessArray = [];
+    guessedAlpha = [];
+
+    for (var i = 0; i < totalGuessLength; i++) {
+        guessArray.push("_");
+    }
+
+    currentWord.innerHTML = guessArray.join(" ");
+    guessTracking.innerHTML = "Total remaining guesses: " + totalGuesses;
+    keyLog.innerHTML = guessedAlpha.join(" ");
+}
+
+function verifyChikaChikaBoomBoom(letter) {
+    if (alphaList.indexOf(letter) === -1) return;
+    if (setArray.has(letter) === true) {
         return;
     } else {
-        lettersArray.add(keyGuess);
+        setArray.add(letter);
     }
-    keyLog.innerHTML += str.toUpperCase() + " ";
-    console.log(str);
+
+    var foundInWord = false;
+
+    for (var i = 0; i < totalGuessLength; i++) {
+        if (wordsRandom[i] == letter) {
+            foundInWord = true;
+        }
+    }
+
+    if (foundInWord) {
+        for (var k = 0; k < totalGuessLength; k++) {
+            if (wordsRandom[k] == letter) {
+                guessArray[k] = letter;
+                guessedAlpha.push(letter.toUpperCase());
+            }
+        }
+    } else {
+        guessedAlpha.push(letter.toUpperCase());
+        totalGuesses--;
+    }
+}
+
+function runRightRoundBaby() {
+    currentWord.innerHTML = guessArray.join(" ");
+    guessTracking.innerHTML = "Total remaining guesses: " + totalGuesses;
+    keyLog.innerHTML = guessedAlpha.join(" ");
+
+    if (wordsRandomStr.toString() === guessArray.toString()) {
+        winWin++;
+        winTracking.innerHTML = winWin;
+        helloDarkness();
+    } else if (totalGuesses === 0) {
+        loseLose++;
+        helloDarkness();
+    }
+}
+
+helloDarkness();
+
+document.addEventListener("keyup", event => {
+    var str = String.fromCharCode(event.keyCode);
+    var strLower = str.toLowerCase();
+
+    // if (alphaList.indexOf(event === -1) {
+    //     return;
+    // }
+    if (setArray.has(event) === true) {
+        return;
+    }
+
+    verifyChikaChikaBoomBoom(strLower);
+    runRightRoundBaby();
 })
 
 
+// document.addEventListener("keyup", event => {
+//     var str = String.fromCharCode(event.keyCode);
+//     var strLower = str.toLowerCase();
+
+//     guessTracking.innerText = "Number of guesses remaining: " + totalGuesses--;
+
+//     if (alphaList.indexOf(keyGuess) === -1) return;
+//     if (lettersArray.has(keyGuess) === true) {
+//         return;
+//     } else {
+//         lettersArray.add(keyGuess);
+//     }
+
+//     if (splitString.indexOf(strLower) != -1) {
+//         for (var i = 0; i < wordsRandom.length; i++) {
+//             if (splitString[i] === strLower) {
+//                 guessArray[i] = strLower;
+//             }
+//         }
+//     }
+//     console.log(guessArray);
+
+//     keyLog.innerHTML += str.toUpperCase() + " ";
+// })
+// console.log(guessArray);
+// // currentWord.innerHTML += guessArray.join(" ");
+
+
+// // for (var i = 0; i < wordsRandom.length; i++) {
+// //     guessArray[i] = "_";
+// //     currentWord.innerText = guessArray.join(" ");
+// //     currentWord.innerText += "\n" + wordsRandom;
+// // }
 
 
 
@@ -53,7 +161,8 @@ document.addEventListener("keyup", event => {
 
 
 
-// var winsTracking = document.getElementById("winsDiv");
+
+// var winTracking = document.getElementById("winsDiv");
 // var currentWord = document.getElementById("currentWordDiv");
 // var guessTracking = document.getElementById("guessesLeftDiv");
 // var keyLog = document.getElementById("guessedLettersDiv");
